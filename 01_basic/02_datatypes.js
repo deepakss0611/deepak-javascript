4bb2b3aec642c2ec21f29d91ed3d8c8dc87f9a09
deepakss0611 <deepakkrsingh61101@gmail.com>
2024-08-16 23:04:58 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are usig nodejs, not brower
+
+console.log( 3
+    +
+    3) // code  readability should be high
+
+console.log("deepak")
+
+
+let name = "messi"
+let age = 37
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+//null =>standalone value
+//undefined =>
+// symbol => unique 
+
+
+//object 
+
+console.lofg(typeof undefined); //undefined
+console.log(typeof null); //object

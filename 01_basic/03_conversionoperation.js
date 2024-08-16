3e9b44d59c214b1f43d75fa80ed5a586c89334f5
deepakss0611 <deepakkrsingh61101@gmail.com>
2024-08-16 23:40:00 +0000

    conversiom of datatype

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"50" => 50
+//"50abc" => NaN
+//true=>1; false=>0
+
+let isLoggedIn = "messi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//""=> false
+//"messi" => true
+
+let someNumber = 50
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);

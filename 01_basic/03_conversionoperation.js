f7a1227453a4d93756b429d9618f6b7828466e68
deepakss0611 <deepakkrsingh61101@gmail.com>
2024-08-17 18:26:24 +0000

    conversion

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -21,8 +21,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //""=> false
 //"messi" => true
 
-let someNumber = 50
+ let someNumber = 50
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*********************** Operations *****************************
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "barcelona"
+let str2 = " messi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log( 1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) *5 % 7)
+
+//  console.log(+true);
+//  console.log(+"");
+
+ let num1, num2, num3
+
+ num1 = num2 = num3 = 2 + 2 
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+
+ // link to study
+ https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
